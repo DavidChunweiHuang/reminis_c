f94a83fc8eca06c8f0a7c2d2a4d7c309b163de4e
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-11 17:46:58 +0800

    fix. move out files in _next/static

--- pages/components/Banner/LeftBanner.tsx
+++ pages/components/Banner/LeftBanner.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-// import logo_pattern from '/reminis_c/public/img/logo/logo_pattern.svg'
+import logo_pattern from '../../../public/img/logo/logo_pattern.svg'
 // import ic_point from '../../../public/img/icon/ic_point@2x.png'
 import styles from '../../../styles/Banner/LeftBanner.module.scss'
 import React from 'react'
@@ -9,7 +9,8 @@ const LeftBanner = ({ promote_text, banner_title }: any) => {
     return (
         <div className={styles.left_container}>
             <div className={styles.main_title}>
-                <img src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/logo/logo_pattern.svg`} width="28.67" height="42" alt={''} />
+                {/* <img src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/logo/logo_pattern.svg`} width="28.67" height="42" alt={''} /> */}
+                <img src={logo_pattern} width="28.67" height="42" alt={''} />
                 <span id={styles.en_subtitle}>{"Reminis Candēre"}</span>
                 <span id={styles.ch_title}>{banner_title}</span>
             </div>
