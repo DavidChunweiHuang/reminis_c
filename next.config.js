40fa9f11ecc3f1c126398b3efa54ed851a10ffee
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-11 19:51:41 +0800

    fix. change path test

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
   basePath: '/reminis_c',  // 替換為你的倉庫名稱
-  assetPrefix: ".",
+  assetPrefix: "/reminis_c/",
   trailingSlash: true,     // 確保所有靜態路徑以斜線結尾
   images: {
     unoptimized: true,    // 禁用圖片優化功能，適用於靜態導出
