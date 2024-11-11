0c568b36ed74520c06bdbfb47340702179c4eecc
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-11 17:19:42 +0800

    fix. 新增 .env 並將圖片的路徑更改為環境變量

--- pages/components/Banner/LeftBanner.tsx
+++ pages/components/Banner/LeftBanner.tsx
@@ -9,7 +9,7 @@ const LeftBanner = ({ promote_text, banner_title }: any) => {
     return (
         <div className={styles.left_container}>
             <div className={styles.main_title}>
-                <img src="/reminis_c/public/img/logo/logo_pattern.svg" width="28.67" height="42" alt={''} />
+                <img src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/logo/logo_pattern.svg`} width="28.67" height="42" alt={''} />
                 <span id={styles.en_subtitle}>{"Reminis Candēre"}</span>
                 <span id={styles.ch_title}>{banner_title}</span>
             </div>
