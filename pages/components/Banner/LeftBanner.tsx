f80668e34b273e47825c1e25ddd36a7c8ded9845
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-11 17:03:10 +0800

    fix. route of img

--- pages/components/Banner/LeftBanner.tsx
+++ pages/components/Banner/LeftBanner.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import logo_pattern from '/reminis_c/public/img/logo/logo_pattern.svg'
-import ic_point from '../../../public/img/icon/ic_point@2x.png'
+// import logo_pattern from '/reminis_c/public/img/logo/logo_pattern.svg'
+// import ic_point from '../../../public/img/icon/ic_point@2x.png'
 import styles from '../../../styles/Banner/LeftBanner.module.scss'
 import React from 'react'
 
@@ -9,13 +9,14 @@ const LeftBanner = ({ promote_text, banner_title }: any) => {
     return (
         <div className={styles.left_container}>
             <div className={styles.main_title}>
-                <img src={logo_pattern} width="28.67" height="42" alt={''} />
+                <img src="/reminis_c/public/img/logo/logo_pattern.svg" width="28.67" height="42" alt={''} />
                 <span id={styles.en_subtitle}>{"Reminis Candēre"}</span>
                 <span id={styles.ch_title}>{banner_title}</span>
             </div>
 
             <div className={styles.icon_main}>
-                <Image src={ic_point} width="26.5" height="1" alt={''} />
+                {/* <Image src={ic_point} width="26.5" height="1" alt={''} /> */}
+                <img src="/reminis_c/public/img/icon/ic_point@2x.png" width="26.5" height="1" alt={''} />
             </div>
 
             <div className={styles.promote_text_area}>
