8be42a95bd2b9ca4228ec99fc1c884d8fefeb44a
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-11 20:35:24 +0800

    fix. change img path test

--- pages/components/Banner/LeftBanner.tsx
+++ pages/components/Banner/LeftBanner.tsx
@@ -9,7 +9,8 @@ const LeftBanner = ({ promote_text, banner_title }: any) => {
     return (
         <div className={styles.left_container}>
             <div className={styles.main_title}>
-                <Image src={logo_pattern} width="28.67" height="42" alt={''} />
+                {/* <Image src={logo_pattern} width="28.67" height="42" alt={''} /> */}
+                <img src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/logo/logo_pattern.svg`} width="26.5" height="1" alt={''} />
                 <span id={styles.en_subtitle}>{"Reminis Candēre"}</span>
                 <span id={styles.ch_title}>{banner_title}</span>
             </div>
