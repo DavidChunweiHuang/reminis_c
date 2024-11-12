5b51b44661734e62af25a6ce15d08710dbb10d73
DavidChunweiHuang <david.chunwei.huang@gmail.com>
2024-11-12 12:06:20 +0800

    fix. change new logo img

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Navigation from './Navigation'
-import logo_all from '../../../public/img/logo/logo_all.svg'
+import logo_all from '../../../public/img/logo/RC_logo_new-02.png'
 import styles from '../../../styles/Header/Header.module.scss'
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
         <div className={styles.container}>
             <div className={styles.header_image}>
                 <Link href="/">
-                    <Image src={logo_all} width='218' height='61' alt={''} />
+                    <Image src={logo_all} fill alt={''} objectFit='contain' />
                 </Link>
 
             </div>
